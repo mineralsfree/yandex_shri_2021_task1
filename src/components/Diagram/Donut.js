e80ff2a534cd9f37c9e8292fd6a4790d5860fd8b
Vasya Pupkin <mineralsfree@gmail.com>
2021-03-19 17:44:29 +0100

    test Diagram 2

--- src/components/Diagram/Donut.js
+++ src/components/Diagram/Donut.js
@@ -5,12 +5,16 @@ import {cn} from "@bem-react/classname";
 
 const DonatCN = cn('donat')
 export const Donut = props => {
-  console.log(props);
   const {height, categories, width, totalText, differenceText} = props;
-  const data = categories.map(el => {
-    return {value: Number(el.valueText.split(' ')[0])}
+  const sum = categories.reduce((a, c)=>Number(c.valueText.split(' ')[0])+a,0);
+  let data = categories.map((el, i) => {
+    let value = Number(el.valueText.split(' ')[0])
+    console.log(value/sum*360);
+    value = true ? (value/sum*360)-1 : value;
+    return {value, id: i}
   })
-  console.log(data);
+      .reverse()
+  // data =   [data[1], data[0], data[3], data[2]]
   return (<div className={DonatCN('container')}>
       <div className={DonatCN('text-container')} style={{width, height}}>
         <div className={DonatCN('text', {total: true})}>{totalText}</div>
@@ -18,28 +22,28 @@ export const Donut = props => {
       </div>
       <PieChart isAnimationActive={false} width={width} height={height}>
         <defs>
-          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint1">
+          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint0">
             <stop offset="71.875%" stop-color="#FFA300"/>
             <stop offset="100%" stop-color="#5B3A00"/>
           </radialGradient>
-          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint3">
+          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint1">
+            <stop offset="72.9167%" stop-color="#633F00"/>
+            <stop offset="100%" stop-color="#0F0900"/>
+          </radialGradient>
+          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint2">
             <stop offset="71.875%" stop-color="#9B9B9B"/>
             <stop offset="100%" stop-color="#382900"/>
           </radialGradient>
-          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint2">
+          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint3">
             <stop offset="71.875%" stop-color="#4D4D4D"/>
             <stop offset="100%" stop-color="#382900"/>
-          </radialGradient>
-          <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint0">
-            <stop offset="72.9167%" stop-color="#633F00"/>
-            <stop offset="100%" stop-color="#0F0900"/>
           </radialGradient>
         </defs>
 
         <Pie
           paddingAngle={1}
-          // startAngle={-2}
-          // endAngle={358}
+            startAngle={120}
+            endAngle={480}
           isAnimationActive={false}
           data={data}
           dataKey="value"
@@ -51,7 +55,8 @@ export const Donut = props => {
 
           {
             data.map((entry, index) => {
-              return <Cell key={`cell-${index}`} fill={`url(#paint${index})`} stroke={'none'}/>
+              console.log(entry, index);
+              return <Cell key={`cell-${index}`} fill={`url(#paint${entry.id})`} stroke={'none'}/>
             })
           }
         </Pie>

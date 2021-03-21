e9303972cd299affd34d92d33bb5fe495bec8238
Vasya Pupkin <mineralsfree@gmail.com>
2021-03-21 14:41:58 +0100

    Diagram test x

--- src/components/Diagram/Donut.js
+++ src/components/Diagram/Donut.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import {Cell, Pie, PieChart} from "recharts";
 import {cn} from "@bem-react/classname";
-import {Yellow, Brown, Grey, DarkGrey} from "./SVGEffects/Dark";
+import {Brown, DarkGrey, Grey, Yellow} from "./SVGEffects/Dark";
 
 
 const DonatCN = cn('donat')
 export const Donut = props => {
   const {height, categories, width, totalText, differenceText} = props;
-  const sum = categories.reduce((a, c)=>Number(c.valueText.split(' ')[0])+a,0);
+  const sum = categories.reduce((a, c) => Number(c.valueText.split(' ')[0]) + a, 0);
   let data = categories.map((el, i) => {
     let value = Number(el.valueText.split(' ')[0])
-    console.log(value/sum*360);
-    value = true ? (value/sum*360)-1 : value;
+    console.log(value / sum * 360);
+    value = true ? (value / sum * 360) - 1 : value;
     return {value, id: i}
   })
-      .reverse()
+    .reverse()
   // data =   [data[1], data[0], data[3], data[2]]
   return (<div className={DonatCN('container')}>
       <div className={DonatCN('text-container')} style={{width, height}}>
@@ -31,13 +31,12 @@ export const Donut = props => {
 
         <Pie
           paddingAngle={1}
-            startAngle={120}
-            endAngle={480}
+          startAngle={120}
+          endAngle={480}
           isAnimationActive={false}
           data={data}
           dataKey="value"
-          cx="50%"
-          cy="50%"
+
           innerRadius={'71.5%'}
           outerRadius={width / 2}
           cornerRadius={6}>
@@ -48,7 +47,7 @@ export const Donut = props => {
               return <Cell
                 key={`cell-${index}`}
                 fill={`url(#paint${entry.id})`}
-                fillOpacity={index===0 ? 0.8 : 0.5}
+                fillOpacity={index === 3 ? 0.8 : 0.5}
                 // filter={`url(#filter${entry.id})`}
                 stroke={'none'}/>
             })
--- src/components/Diagram/SVGEffects/Dark.js
+++ src/components/Diagram/SVGEffects/Dark.js
@@ -2,42 +2,18 @@ import React from 'react'
 
 export const Yellow = () => {
   return (<>
-    <filter xmlns="http://www.w3.org/2000/svg" id="filter0" filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB">
-      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-      <feMorphology radius="8" operator="erode" in="SourceAlpha" result="effect1_dropShadow"/>
-      <feOffset/>
-      <feGaussianBlur stdDeviation="10"/>
-      <feColorMatrix type="matrix" values="0 0 0 0 0.972549 0 0 0 0 0.618715 0 0 0 0 0 0 0 0 0.2 0"/>
-      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                     result="hardAlpha"/>
-      <feOffset/>
-      <feGaussianBlur stdDeviation="10"/>
-      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-      <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.636666 0 0 0 0 0 0 0 0 0.9 0"/>
-      <feBlend mode="normal" in2="shape" result="effect2_innerShadow"/>
-      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                     result="hardAlpha"/>
-      <feOffset dx="-1" dy="1"/>
-      <feGaussianBlur stdDeviation="0.5"/>
-      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-      <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"/>
-      <feBlend mode="normal" in2="effect2_innerShadow" result="effect3_innerShadow"/>
-    </filter>
-    <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint0">
-      <stop offset="71.875%" stop-color="#FFA300"/>
-      <stop offset="100%" stop-color="#5B3A00"/>
+    <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint0" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(160.4859 166.514) rotate(0) scale(163.486)">
+      <stop offset="0.71875" stop-color="#FFA300"/>
+      <stop offset="1" stop-color="#5B3A00"/>
     </radialGradient>
   </>)
 }
 export const Brown = () => {
   return (<>
-      <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint1">
-        <stop offset="72.9167%" stop-color="#633F00"/>
-        <stop offset="100%" stop-color="#0F0900"/>
+      <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint1" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse"
+                      gradientTransform="translate(163.22 164.24) rotate(90) scale(163.486)">
+        <stop offset="0.729167" stop-color="#633F00"/>
+        <stop offset="1" stop-color="#0F0900"/>
       </radialGradient>
       <filter xmlns="http://www.w3.org/2000/svg" id="filter1"
               filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
@@ -68,10 +44,11 @@ export const Brown = () => {
   )
 }
 export const Grey = ()=>{
-  return (<><radialGradient xmlns="http://www.w3.org/2000/svg" id="paint2">
-        <stop offset="71.875%" stop-color="#9B9B9B"/>
-        <stop offset="100%" stop-color="#382900"/>
-      </radialGradient>
+  return (<>        <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint2" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(163.67 164.5893924331665) rotate(90) scale(163.486)">
+      <stop offset="0.71875" stop-color="#9B9B9B"/>
+      <stop offset="1" stop-color="#382900"/>
+    </radialGradient>
+
       <filter xmlns="http://www.w3.org/2000/svg" id="filter2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
         <feFlood flood-opacity="0" result="BackgroundImageFix"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
@@ -100,9 +77,9 @@ export const Grey = ()=>{
 export const DarkGrey = ()=>{
   return (<>
 
-    <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint3">
-      <stop offset="71.875%" stop-color="#4D4D4D"/>
-      <stop offset="100%" stop-color="#382900"/>
+    <radialGradient xmlns="http://www.w3.org/2000/svg" id="paint3" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(163.486 164.514) rotate(90) scale(163.486)">
+      <stop offset="0.71875" stop-color="#4D4D4D"/>
+      <stop offset="1" stop-color="#382900"/>
     </radialGradient>
     <filter xmlns="http://www.w3.org/2000/svg" id="filter3"  filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
       <feFlood flood-opacity="0" result="BackgroundImageFix"/>

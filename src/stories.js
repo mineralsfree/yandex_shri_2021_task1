dff4c38210d594e9eed7f43604d4881d3fa6ca03
Vasya Pupkin <mineralsfree@gmail.com>
2021-03-22 12:56:07 +0100

    fixfix

--- src/stories.js
+++ src/stories.js
@@ -3,9 +3,7 @@ import ReactDOMServer from 'react-dom/server'
 import './_colors.scss'
 import './style.scss'
 import {App} from './App'
-import {data} from './data'
 function renderTemplate(kek, lol) {
-  const item = data[2]
-  return ReactDOMServer.renderToStaticMarkup(<App alias={item.alias} data={item.data}/>)
+  return ReactDOMServer.renderToStaticMarkup(<App alias={kek} data={lol}/>)
 }
 window.renderTemplate = renderTemplate;
